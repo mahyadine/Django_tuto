e9f3d1cb05824123a1208d49a6f9f93d906fe85a
mahyadine <yassimahyadine@gmail.com>
2020-03-20 16:37:15 +0100

    create view home/templates

--- tutoriel/home/views.py
+++ tutoriel/home/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello my first page')
+    return render(request, 'home/index.html')
